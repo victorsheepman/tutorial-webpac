c7c966715a63569905567239a28b4b7077a802cc
Victor Marquez <chirlychplin@gmail.com>
2021-04-09 13:10:20 -0400

    erro solucionado

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpack = require('copy-webpack-plugin'); 
+const { patch } = require('semver');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+//las Exportaciones
+module.exports = {
+    entry:'./src/index.js',//Entry nos permite saber el archivo que compilara webpack
+    output: {//el  objeto output se refiere al archivo ya compilado
+        filename: '[hash].main.js',//nombre del archivo js compilado
+        path: path.resolve(__dirname, 'dist')//carpeta donde estara los archivos optimizados y compilados
+    },
+    resolve:{
+        extensions: ['.js']//indicamos la extencion
+    },
+    module: {//en module se añaden los loader(los loader pre-procesa el codigo fuente y lo devuelve transformafo)
+        
+        rules: [//En rules se añaden todas las reglas de los loader
+           
+            //reglas de babel.
+            {
+        
+                test: /\.m?js$/,//se declara la extencion de los archivos que aplaicara el loader
+               
+                exclude: /node_modules/,
+               
+                use: {//use es objeto donde estara el loader a utilizar.
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [MiniExtractPlugin.loader,'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use:['file-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3000,
+      },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniExtractPlugin(),
+        new CopyWebpack({
+            patterns:[
+                {
+                    from: path.resolve(__dirname, 'src', 'assets/image'),
+                    to: "assets/image"
+                }
+            ]
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+          new CssMinimizerPlugin(),
+          new TerserPlugin()
+        ]
+    },
+}
